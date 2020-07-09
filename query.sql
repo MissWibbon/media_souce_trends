37326efac0398eb62d3f1b36873ef2305b808aa0
GracieX <41657451+GracieX@users.noreply.github.com>
2020-07-08 22:36:56 -0500

    query

---
+++ query.sql
@@ -0,0 +1,18 @@
+drop table president;
+
+CREATE TABLE president (
+year VARCHAR(10),	
+state VARCHAR(30),	
+state_po VARCHAR(30),
+state_cen INT,	
+state_ic INT,
+office VARCHAR(30),
+candidate VARCHAR(100),
+party VARCHAR(30),	
+writein	VARCHAR(30),
+candidatevotes INT,
+totalvotes INT
+);
+
+
+SELECT * FROM president;
